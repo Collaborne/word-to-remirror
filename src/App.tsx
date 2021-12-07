69c4c2832f2ac3e7b2398d58a1479b289be2b688
Ronny Roeller <ronny@collaborne.com>
2021-12-07 17:04:55 +0100

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import { SocialEditor } from '@remirror/react-editors/social';
+const USERS = [
+  { id: 'joe', label: 'Joe' },
+  { id: 'john', label: 'John' },
+];
+const TAGS = ['remirror', 'editor'];
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ padding: 16 }}>
+      <SocialEditor users={USERS} tags={TAGS} />
     </div>
   );
 }
-
 export default App;
